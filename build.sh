257a0e25f204a8cca193c6ce3117cd52a1957c15
Jan Sedivy <jansedivy@abdoc.net>
2015-06-15 19:35:46 +0200

    use dynamically loaded sdl library

--- build.sh
+++ build.sh
@@ -6,4 +6,4 @@ mkdir -p build/raytracer.app/Contents/MacOS
 mkdir -p build/raytracer.app/Contents/Resources
 
 clang++ -dynamiclib src/app.cpp -o build/raytracer.app/Contents/Resources/app.dylib -g -O2 -ffast-math
-clang++ src/osx_main.cpp -o build/raytracer.app/Contents/MacOS/raytracer -lSDL2 -g -O2 -framework OpenGl
+clang++ src/osx_main.cpp -o build/raytracer.app/Contents/MacOS/raytracer -I./build/raytracer.app/Contents/Frameworks/Headers -F ./build/raytracer.app/Contents/Frameworks -framework SDL2 -O2 -framework OpenGl -rpath @executable_path/../Frameworks
